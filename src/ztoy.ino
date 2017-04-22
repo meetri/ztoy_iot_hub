d07b32f13f952e838aa3447e8e21ac1f92b7e7da
demetrius bell <meetri@gmail.com>
2017-04-21 18:44:50 -0700

    add support for display

--- src/ztoy.ino
+++ src/ztoy.ino
@@ -1,3 +1,6 @@
+#include <Adafruit_SSD1306.h>
+#include <Wire.h>
+#include <SPI.h>
 #include <circlebuffer.h>
 #include <rawplayer.h>
 
@@ -6,10 +9,14 @@
 #define SPEAKER_BUFFER_SIZE 1024
 #define MICROPHONE_BUFFER_SIZE 2048
 #define LED1 D7
-#define BTN1 D4
+#define BTN1 D5
 #define SPEAKER_PIN DAC1
 #define SEND_OVER_TCP 1
 #define BEATDELAY 500000
+
+//======OLED=========
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
 
 
 IPAddress server = IPAddress(192,168,86,179);
@@ -21,25 +28,14 @@ Rawplayer *player;
 Circlebuffer speakerBuffer;
 Circlebuffer micBuffer;
 
-unsigned long stopFrame;
-
-/*
- * for speach detection feature 
- */
-unsigned long lastsample = 0;
-unsigned long lastbeat = 0;
-bool ok = false;
-
-int ms = 125;
-double silentThreshold = 0;
-int pages = 0;
-
 
 void setup() {
     Serial.begin(115200);
 
     pinMode(LED1,OUTPUT);
     pinMode(BTN1,INPUT_PULLDOWN);
+
+    display.begin(SSD1306_SWITCHCAPVCC, 0x3C);
 
     Serial.println("Waiting for wifi...");
     while (!WiFi.ready()) Particle.process();
@@ -52,8 +48,94 @@ void setup() {
     player = new Rawplayer();
     player->begin( tcpsocket, speakerBuffer, SPEAKER_PIN );
 
+    display.display();
+    delay(250);
+
+    display.clearDisplay();
+
 }
 
+/*
+ * for speach detection feature 
+ */
+unsigned long lastsample = 0;
+unsigned long lastbeat = 0;
+bool ok = false;
+
+int ms = 125;
+double silentThreshold = 0;
+int pages = 0;
+
+void loop(){
+
+    if ( !ok ){
+
+        if ( tcpsocket->available() > 0 ){
+            display.clearDisplay();
+            display.setTextSize(1);
+            display.setTextColor(WHITE);
+            display.setCursor(0,0);
+            display.println("Status: Playing Stream");
+            display.display();
+            if (player->recieveAndPlay()){
+                digitalWrite(SPEAKER_PIN,0);
+            }
+        }
+
+        silentThreshold = getAmbiantNoiseAverage(50);
+        ok = digitalRead(BTN1) == HIGH;
+
+        if ( ok ){
+            display.clearDisplay();
+            display.setTextSize(1);
+            display.setTextColor(WHITE);
+            display.setCursor(0,0);
+            display.println("Status: Stream Recording");
+            display.display();
+        }else {
+            display.clearDisplay();
+            display.setTextSize(1);
+            display.setTextColor(WHITE);
+            display.setCursor(0,0);
+            display.println("Status: Idle");
+            display.print("Volts: ");
+            display.println( silentThreshold );
+            display.display();
+        }
+        
+    }
+
+    while ( ok ) {
+
+        unsigned long beat = micros();
+        if ( beat - lastbeat > BEATDELAY ){
+            ok = false;
+            lastbeat = beat;
+            if ( WiFi.ready() && digitalRead(BTN1) == HIGH ) {
+            //if ( WiFi.ready() && threshold >= 0.1 ) {
+                if ( !tcpsocket->connected() ){
+                    if ( tcpsocket->connect( server , servicePort ) ){
+                        Serial.println("connected");
+                        ok = true;
+                    }
+                }else {
+                    Serial.println("still connected");
+                    ok = true;
+                }
+            }else {
+                Serial.println("Stopped recording");
+            }
+        }
+
+        if ( beat - lastsample > 125 ){
+            if ( readMic16( &micBuffer ) ){
+                sendAudio( &micBuffer );
+            }
+            lastsample = beat;
+        }
+    }
+
+}
 
 double getAmbiantNoiseAverage(unsigned int sampleWindow){
 
@@ -88,46 +170,6 @@ double getAmbiantNoiseAverage(unsigned int sampleWindow){
     return volts;
 }
 
-void loop(){
-
-    if ( !ok ){
-        if (player->recieveAndPlay()){
-            digitalWrite(SPEAKER_PIN,0);
-        }
-        ok = digitalRead(BTN1) == HIGH;
-    }
-
-    while ( ok ) {
-
-        unsigned long beat = micros();
-        if ( beat - lastbeat > BEATDELAY ){
-            ok = false;
-            lastbeat = beat;
-            if ( WiFi.ready() && digitalRead(BTN1) == HIGH ) {
-            //if ( WiFi.ready() && threshold >= 0.1 ) {
-                if ( !tcpsocket->connected() ){
-                    if ( tcpsocket->connect( server , servicePort ) ){
-                        Serial.println("connected");
-                        ok = true;
-                    }
-                }else {
-                    Serial.println("still connected");
-                    ok = true;
-                }
-            }else {
-                Serial.println("Stopped recording");
-            }
-        }
-
-        if ( beat - lastsample > 125 ){
-            if ( readMic16( &micBuffer ) ){
-                sendAudio( &micBuffer );
-            }
-            lastsample = beat;
-        }
-    }
-
-}
 
 // ------------------------------------------------------------------------------------------
 // STREAM FROM MICROPHONE
